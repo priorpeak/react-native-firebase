8f2d15453b11ba35f3e38126387bc4fbd21f785e
Eric Benfaida <benfaida@bu.edu>
2021-09-14 17:24:21 -0400

    Recipe Array and Buttons for Input

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
-import { TextInput, View, TouchableOpacity, Text } from "react-native";
+import { TextInput, View, TouchableOpacity, Text, Button } from "react-native";
 import styles from "./styles";
 // import * as firebase from "firebase";
 import { collection, getDocs } from "firebase/firestore";
@@ -8,6 +8,15 @@ import { collection, getDocs } from "firebase/firestore";
 export default function HomeScreen({ navigation }) {
   const [numServings, setNumServings] = useState("");
   const [recipeName, setRecipeName] = useState("");
+  const [recipeArray, setRecipeArray] = useState([]);
+
+  const addRecipe = () => {
+    setRecipeArray([...recipeName, {
+      id: recipeArray.length,
+      value: recipeName
+    }])
+  }
+
   let totalCalories = "345";
 
   const documentQuery = await getDocs(collection(db, "Foods"));
@@ -26,6 +35,7 @@ export default function HomeScreen({ navigation }) {
         placeholder="1"
         onChangeText={(val) => setNumServings(val)}
       />
+        <Button title = "Enter"/>
 
       <Text>Enter Recipe Name (leave blank if a la carte):</Text>
 
@@ -35,6 +45,8 @@ export default function HomeScreen({ navigation }) {
         placeholder="Mac and Cheese"
         onChangeText={(val) => setRecipeName(val)}
       />
+        <Button onClick ={addRecipe} title = "Enter"/>
+
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate("Barcode")}
